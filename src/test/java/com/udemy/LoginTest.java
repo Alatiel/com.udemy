ffe0e72c97c99cf025e9384c1a51d7eac364d283
inna <NOfate281>
2020-05-19 17:04:41 +0300

    Remove Login test with credentials from test suite(testng.xml). Add a new login test with cookies.

--- src/test/java/com/udemy/LoginTest.java
+++ src/test/java/com/udemy/LoginTest.java
@@ -1,5 +1,6 @@
 package com.udemy;
 
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -8,34 +9,17 @@ import org.openqa.selenium.By;
 
 public class LoginTest extends BaseTest {
 
-    @Test (description = "Login with valid data")
+    @Test (description = "Login with Cookies")
     public void loginTest(){
 
-        By headerLoginBtnLocator = By.xpath("//button[@data-purpose='header-login']");
-        By loginEmailFieldLocator = By.xpath("//input[@name='email']");
-        By loginPasswordFieldLocator = By.xpath("//input[@type='password']");
-        By loginBtnLocator = By.xpath("//input[@type='submit']");
         By userLabelLocator = By.xpath("//span[@class='user-initials']");
 
         driver.get("https://www.udemy.com/");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(headerLoginBtnLocator));
-        WebElement headerLoginBtn = driver.findElement(headerLoginBtnLocator);
-        headerLoginBtn.click();
+        Cookie userLogin = new Cookie("dj_session_id","ue5va8vru50qk5cxbhy4614u9xbxxpm7");
+        driver.manage().addCookie(userLogin);
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(loginEmailFieldLocator));
-        WebElement loginEmailField = driver.findElement(loginEmailFieldLocator);
-        loginEmailField.clear();
-        loginEmailField.sendKeys("lm1b@appcreative.net");
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(loginPasswordFieldLocator));
-        WebElement loginPasswordField = driver.findElement(loginPasswordFieldLocator);
-        loginPasswordField.clear();
-        loginPasswordField.sendKeys("@LM123456");
-
-        WebElement loginBtn = driver.findElement(loginBtnLocator);
-        wait.until(ExpectedConditions.elementToBeClickable(loginBtnLocator));
-        loginBtn.click();
+        driver.navigate().refresh();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(userLabelLocator));
         WebElement userLabel = driver.findElement(userLabelLocator);
